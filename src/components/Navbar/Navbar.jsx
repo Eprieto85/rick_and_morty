7f0e5bb7f4df409d020b1b106419ded61e8f8ebb
Eprieto85 <109884267+Eprieto85@users.noreply.github.com>
2022-12-18 16:57:27 -0500

    cambios en el navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,55 +18,83 @@
 // );
 // }
 
-import React from 'react'
-import { NavLink,Link } from "react-router-dom";//,useNavigate
+import React,{ useState } from 'react'
+import { NavLink, useNavigate } from "react-router-dom";//,useNavigate
 import '../../App.css';
 import './Navbar.css'
 
 const Navbar = () => {
-  // const navigate=useNavigate();
+// evento para responsive del navbar
+const[click,setClick] =useState(false);
+const handleClick =()=>setClick(!click);
+
+// evento para el logout
+const navegacion=useNavigate();
+const logout=()=>{
+   navegacion('/login',{replace:true})
+}
+
+
+  // const navigate=useNavigate();  eliminar la funcion q continua y dejar del return en adelante
 // para no mostrar el boton LogOut cuando salga del app
-  function hideButton(){
-    document.getElementById('logOut').style.display='none';
-  }
+  // function hideButton(){
+  //   document.getElementById('logOut').style.display='none';
+  // }
 
   return (
-    <nav className="navbar navbar-expand-lg bg-light mb-4" onClick={hideButton}>
-  <div className="container">
+    <>
+    <nav className='navbar'>
+    {/* <nav className="navbar navbar-expand-lg bg-light mb-4" onClick={hideButton}> */}
+  {/* <div className="container">para eliminar al final */}
   <h1 className="nav-logo-img" >
   <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/128px-Rick_and_Morty.svg.png' alt="logo-Rick&Morty"/>
   </h1>
-  <Link to="/" className="fs-3 ubuntu navbar-brand">Rick & <span className='text-primary'>Morty</span>  </Link>     
+  {/* <Link to="/" className="fs-3 ubuntu navbar-brand">{/* para eliminar al final */}
+    {/* Rick & <span className='text-primary'>Morty</span>  </Link>      */} 
   
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <style jsx>
-        {`
+    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> */}
+    {/* <style jsx> */}
+        {/* {`
         button[aria-expanded="false"]>.close{
             display: none;
         }
         button[aria-expanded="true"]>.open{
             display: none;
         }
-        `}
-        </style>    
+        `} */}
+        {/* </style>     */}
       {/* <span className="navbar-toggler-icon"></span> cambie los iconos*/} 
-      <i class="fas fa-bars open fw-blod text-dark"></i>
-      <i class="fas fa-times close fw-blod text-dark"></i>
-    </button>
-    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-      <div className="navbar-nav fs-5">
-        <NavLink activeClassName="active" to="/" className="nav-link">Home</NavLink>
+      {/* <i class="fas fa-bars open fw-blod text-dark"></i> */}
+      {/* <i class="fas fa-times close fw-blod text-dark"></i> */}
+    {/* </button> */}
+    {/* <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup"> */}
+      {/* <div className="navbar-nav fs-5"> */}
+        {/* <NavLink activeClassName="active" to="/" className="nav-link">Home</NavLink> */}
         {/* <NavLink to="/home" className="nav-link">Home</NavLink> qda en blanco */}
-        <NavLink to="/mycards" className="nav-link">My Cards</NavLink>
-        <NavLink to="/characters" className="nav-link">Characters</NavLink>
-        <NavLink to="/episodes" className="nav-link">Episodes</NavLink>
-        <NavLink to="/location" className="nav-link">Location</NavLink>
-        <NavLink to="/about" className="nav-link">About</NavLink>
+        {/* <NavLink to="/mycards"    className="nav-link">My Cards   </NavLink> */}
+        {/* <NavLink to="/characters" className="nav-link">Characters </NavLink> */}
+        {/* <NavLink to="/episodes"   className="nav-link">Episodes   </NavLink> */}
+        {/* <NavLink to="/location"   className="nav-link">Location   </NavLink> */}
+        {/* <NavLink to="/about"      className="nav-link">About      </NavLink> */}
         {/* <NavLink to="/logout" className="logOff nav-link" id="logOut" onClick={()=>navigate("/")} style={{display:"none"?"none":"none"}}>Logout</NavLink> */}
-      </div>
-    </div>
-  </div>
+      {/* </div> */}
+    {/* </div> */}
+  {/* </div> */}
+  <ul className={click?'nav-menu active':'nav-menu'}>
+    <li className='nav-item'><NavLink to="/mycards"    activeClassName="active" className="nav-link" onClick={handleClick}>My Cards    </NavLink></li>
+    <li className='nav-item'><NavLink to="/characters" activeClassName="active" className="nav-link" onClick={handleClick}>Characters  </NavLink></li>
+    <li className='nav-item'><NavLink to="/episodes"   activeClassName="active" className="nav-link" onClick={handleClick}>Episodes    </NavLink></li>
+    <li className='nav-item'><NavLink to="/location"   activeClassName="active" className="nav-link" onClick={handleClick}>Location    </NavLink></li>
+    <li className='nav-item'><NavLink to="/about"      activeClassName="active" className="nav-link" onClick={handleClick}>About       </NavLink></li>
+    <li className='nav-btn'><button  className="nav-link" onClick={logout}>LogOut</button></li>
+</ul>
+
+  <div className="nav-icon" onClick={handleClick}>
+    <i className={click?"fas fa-times":"fas fa-bars"}></i>
+</div>
+
 </nav>
+</>
   )
 };
 
