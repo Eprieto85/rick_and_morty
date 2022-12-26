0fd36a8d7c599c42b344d6a0b76372f26540ac01
Eprieto85 <109884267+Eprieto85@users.noreply.github.com>
2022-12-25 21:47:14 -0500

    Esta casi listo

--- src/Pages/Private/Dashboard/Dashboard.jsx
+++ src/Pages/Private/Dashboard/Dashboard.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 
-function Dashboard() {
+const Dashboard = () => {
   return (
-    <div>
-      
-    </div>
+   
+    <div className="d-flex ubuntu justify-content-center fs-5 mb-4">
+    {/* // Este es mi "return statement" elimine todo y creo desde cero style={{ padding: '25px' }}->se borro del div  */}
+<div className="App text-center ubuntu">
+Gracias por llegar hasta aquí!!
+
+<h1 className="text-center mb-4">Te explicare rapidamente el contenido de este tablero{/* Personajes */}</h1>
+<h1 className="text-center ubuntu my-1">Aun estoy haciendo cambios!!</h1>
+
+<span className='text-primary'>Gracias por entender!!</span>
+
+<div className="container">
+<div className='row'>
+<div className="col-2">
+<a title="Curso Barista" href='https://go.hotmart.com/L77323984K'><img src='http://drive.google.com/uc?export=view&id=1eOkD4ky_w89YJtC2SfwaYHhIQAGvHSpU' width="90%" height="70%" alt="curso-barista" /></a>
+dentro del filters
+
+</div>
+ <div className="col-lg-8 col-12">
+     <div className="row">este un area privada, donde tienes acceso a enlaces segun la preferencia del cliente. En este ejemplo son cartas sobre la serie Rick and Morty, entonces podras ver las tarjetas, buscar las que te llamen la atención en el enlace Cards
+     <br/><br/>
+     <p>En personajes puede seleccionar tus favoritas, en espisodios puedes saber que personajes aparecenen cada episodio, </p> 
+     <br/>
+</div>
+Pagination
+</div>          {/* 1 */}
+ </div>          {/* 2 */}
+     </div>          {/* 3 */}
+</div>                 {/* 4 */}
+</div>
   )
 }
-
 export default Dashboard
